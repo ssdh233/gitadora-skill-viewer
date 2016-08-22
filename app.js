e2786ecfd99101e29a1199e423c08b348fab0756
matsumatsu233 <cuizhen199209@gmail.com>
2016-08-22 16:00:55 +0900

    temp

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Example app listening on port 3000!');
+app.use(bodyParser());
+
+// dynamically include routes (Controller)
+fs.readdirSync('./controllers').forEach(function (file) {
+  if(file.substr(-3) == '.js') {
+    route = require('./controllers/' + file);
+    route.controller(app);
+  }
 });
 
+app.listen(3000, function (){
+  console.log('This app listening on port 3000!');
+});
---
+++ controllers/upload_controller.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var pg = require('pg');
+
+module.exports.controller = function (app) {
+  app.post('/upload', function (req, res) {
+    pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      //TODO check whether user id has already existed or not
+      var skill_data_str = JSON.stringify(req.body.skill_data);
+      var sql = 'insert into skill values (1, $$' + skill_data_str + '$$);';
+      client.query(sql, function(err, result) {
+        done();
+
+        if (err) {
+          console.error(err);
+          res.send("Error" + err);
+        } else {
+          res.send("Successfully uploaded.");
+        }
+      });
+    });
+  });
+}
