cc40a385b1bbfff2542c92c7e4d276c61c41562f
matsumatsu233 <cuizhen199209@gmail.com>
2016-09-15 23:20:44 +0900

    Update index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
+  
   $('#sub1').click( function(){
     $('#block1').slideToggle();
   });
+
+  $('#sub2').click( function(){
+    $('#block2').slideToggle();
+  });
+  
+  $('#sub3').click( function(){
+    $('#block3').slideToggle();
+  });
+
+  $('#sub4').click( function(){
+    $('#block4').slideToggle();
+  });
+
 });
