e89c4e94c6ea083533c7724c3dc654ad93ae08de
matsumatsu233 <cuizhen199209@gmail.com>
2016-12-09 13:11:04 +0900

    Add new g/d controller to adapt new version

---
+++ controllers/drum_controller.js
@@ -0,0 +1,59 @@
+var pg = require('pg');
+
+module.exports.controller = function (app) {
+  app.get('/:ver/:id/d', function (req, res) {
+    var skill_table_name;
+    var skillp_table_name;
+
+    switch (req.params.ver) {
+      case "tb":
+        skill_table_name = "skill_tb";
+        skillp_table_name = "skillp_tb";
+        break;
+      default:
+        res.send("Unexpected version name");
+    }
+
+    //pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+      var sql = 'select * from ' + skill_table_name + ' where id =' + req.params.id + ';';
+      client.query(sql, function (err, result) {
+        done();
+
+        if (err) {
+          console.error(sql);
+          console.error(err);
+          res.send(sql + "<br>" + err);
+        } else {
+          if (!result.rows[0]) {
+            res.render("drum");
+          } else {
+            var result_skill = result.rows[0];
+            sql = 'select * from ' + skillp_table_name + ' where skill_id =' + req.params.id + ' and type = $$drum$$;';
+            client.query(sql, function (err, result) {
+              done();
+
+              if (err) {
+                console.error(sql);
+                console.error(err);
+                res.send(sql + "<br>" + err);
+              } else {
+                var skill_data = JSON.parse(result_skill.drum_skill);
+                var skill_point = (parseFloat(skill_data.hot.point) + parseFloat(skill_data.other.point)).toFixed(2);
+                res.render("drum" , {
+                  version: req.params.ver,
+                  player_name : result_skill.player_name.replace(/^"(.*)"$/, '$1'),
+                  id : req.params.id,
+                  skill_data : skill_data,
+                  skill_point : skill_point,
+                  update_date : result_skill.update_date,
+                  skillp_data : result.rows
+                });
+              }
+            });
+          }
+        }
+      });
+    });
+  });
+}
