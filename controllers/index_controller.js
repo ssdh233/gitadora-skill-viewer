2798bf3cc71e41a053097259bd4575961ae0cf35
matsumatsu233 <cuizhen199209@gmail.com>
2016-12-15 17:03:42 +0900

    Add Chinese index page

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -2,6 +2,12 @@ var pg = require('pg');
 
 module.exports.controller = function (app) {
   app.get('/', function (req, res) {
-    res.render("index");
+    res.render("index_jp");
+  });
+  app.get('/jp', function (req, res) {
+    res.render("index_jp");
+  });
+  app.get('/cn', function (req, res) {
+    res.render("index_cn");
   });
 }
