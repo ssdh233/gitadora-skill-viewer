a301ce4c8448b672e703920291454408cd275461
matsumatsu233 <cuizhen199209@gmail.com>
2017-01-18 22:18:42 +0900

    Bug fixed

--- controllers/skill_controller.js
+++ controllers/skill_controller.js
@@ -1,87 +1,90 @@
 var pg = require('pg');
 
 module.exports.controller = function (app) {
-  app.get('/:ver/:id/:type/', function (req, res) {
-    var skill_table_name;
-    var skillp_table_name;
-    var version_name;
-    var type_name;
+  app.get('/:ver/:id/g', function (req, res) {
+    getSkill(req, res, 'g');
+  });
 
-    switch (req.params.type) {
-      case "d":
-        type_name = "drum";
-        break;
-      case "g":
-        type_name = "guitar";
-        break;
-      default:
-        res.send("Unexpected type name");
-    }
+  app.get('/:ver/:id/d', function (req, res) {
+    getSkill(req, res, 'd');
+  });
+}
 
-    switch (req.params.ver) {
-      case "tb":
-        skill_table_name = "skill_tb";
-        skillp_table_name = "skillp_tb";
-        version_name = "GITADORA Tri-Boost";
-        break;
-      case "tbre":
-        skill_table_name = "skill_tbre";
-        skillp_table_name = "skillp_tbre";
-        version_name = "GITADORA Tri-Boost Re:EVOLVE";
-        break;
-      default:
-        res.send("Unexpected version name");
-    }
+function getSkill(req, res, type) {
+  var skill_table_name;
+  var skillp_table_name;
+  var version_name;
+  var type_name;
 
-    //pg.defaults.ssl = true;
-    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-      var sql = 'select * from ' + skill_table_name + ' where id =' + req.params.id + ';';
-      client.query(sql, function (err, result) {
-        done();
+  if (type == "d") {
+    type_name = "drum";
+  } else {
+    type_name = "guitar";
+  }
 
-        if (err) {
-          console.error(sql);
-          console.error(err);
-          res.send(sql + "<br>" + err);
+  switch (req.params.ver) {
+    case "tb":
+      skill_table_name = "skill_tb";
+      skillp_table_name = "skillp_tb";
+      version_name = "GITADORA Tri-Boost";
+      break;
+    case "tbre":
+      skill_table_name = "skill_tbre";
+      skillp_table_name = "skillp_tbre";
+      version_name = "GITADORA Tri-Boost Re:EVOLVE";
+      break;
+    default:
+      res.send("Unexpected version name");
+  }
+
+  //pg.defaults.ssl = true;
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    var sql = 'select * from ' + skill_table_name + ' where id =' + req.params.id + ';';
+    client.query(sql, function (err, result) {
+      done();
+
+      if (err) {
+        console.error(sql);
+        console.error(err);
+        res.send(sql + "<br>" + err);
+      } else {
+        if (!result.rows[0]) {
+          //tmp
+          res.render("skill");
         } else {
-          if (!result.rows[0]) {
-            //tmp
-            res.render("skill");
-          } else {
-            var result_skill = result.rows[0];
-            sql = 'select * from ' + skillp_table_name + ' where skill_id =' + req.params.id + ' and type = $$' + type_name + '$$;';
-            client.query(sql, function (err, result) {
-              done();
+          var result_skill = result.rows[0];
+          sql = 'select * from ' + skillp_table_name + ' where skill_id =' + req.params.id + ' and type = $$' + type_name + '$$;';
+          client.query(sql, function (err, result) {
+            done();
 
-              if (err) {
-                console.error(sql);
-                console.error(err);
-                res.send(sql + "<br>" + err);
+            if (err) {
+              console.error(sql);
+              console.error(err);
+              res.send(sql + "<br>" + err);
+            } else {
+              var skill_data;
+              if (type_name == "drum"){
+                skill_data = JSON.parse(result_skill.drum_skill);
               } else {
-                var skill_data;
-                if (type_name == "drum"){
-                  skill_data = JSON.parse(result_skill.drum_skill);
-                } else {
-                  skill_data = JSON.parse(result_skill.guitar_skill);
-                }
-
-                var skill_point = (parseFloat(skill_data.hot.point) + parseFloat(skill_data.other.point)).toFixed(2);
-                res.render("skill" , {
-                  version : req.params.ver,
-                  version_full : version_name,
-                  player_name : result_skill.player_name.replace(/^"(.*)"$/, '$1'),
-                  id : req.params.id,
-                  skill_data : skill_data,
-                  skill_point : skill_point,
-                  update_date : result_skill.update_date,
-                  skillp_data : result.rows,
-                  type : (type_name == "drum") ? 0 : 1 //1:guitar 0:drum
-                });
+                skill_data = JSON.parse(result_skill.guitar_skill);
               }
-            });
-          }
+
+              var skill_point = (parseFloat(skill_data.hot.point) + parseFloat(skill_data.other.point)).toFixed(2);
+              res.render("skill" , {
+                version : req.params.ver,
+                version_full : version_name,
+                player_name : result_skill.player_name.replace(/^"(.*)"$/, '$1'),
+                id : req.params.id,
+                skill_data : skill_data,
+                skill_point : skill_point,
+                update_date : result_skill.update_date,
+                skillp_data : result.rows,
+                type : (type_name == "drum") ? 0 : 1 //1:guitar 0:drum
+              });
+            }
+          });
         }
-      });
+      }
     });
   });
 }
--- controllers/skill_controller_old.js
+++ controllers/skill_controller_old.js
@@ -1,7 +1,11 @@
 var pg = require('pg');
 
 module.exports.controller = function (app) {
-  app.get('/:id/:type', function (req, res) {
-    res.redirect('/tb/'+req.params.id+"/"+req.params.type);
+  app.get('/:id/d', function (req, res) {
+    res.redirect('/tb/'+req.params.id+"/d");
+  });
+
+  app.get('/:id/g', function (req, res) {
+    res.redirect('/tb/'+req.params.id+"/g");
   });
 }
