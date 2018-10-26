cd431c3058776d8186c251ae609c6f148df2f95b
matsumatsu233 <cuizhen199209@gmail.com>
2018-10-26 22:18:20 +0900

    v1.9.3

--- src/react/Index.jsx
+++ src/react/Index.jsx
@@ -11,7 +11,7 @@ import Popover from "material-ui/Popover";
 
 import SlideToggle from "./SlideToggle.jsx";
 
-const VERSION = "v1.9.2";
+const VERSION = "v1.9.3";
 
 class Index extends React.Component {
   constructor(props) {
