d85b435b3a4bf64556fb81227a6c25e0869e5e0b
matsumatsu233 <cuizhen199209@gmail.com>
2018-11-22 23:42:30 +0900

    Add title for kasegi page

--- src/react/KasegiPage.jsx
+++ src/react/KasegiPage.jsx
@@ -67,31 +67,30 @@ class KasegiPage extends React.Component {
       }
     } = this.props;
 
-    const typeTitle = type === "d" ? "DRUM" : "GUITAR";
+    const typeTitle = type === "d" ? "Drummania" : "Guitarfreaks";
+    const typeTitleShort = type === "d" ? "DRUM" : "GUITAR";
     const scopeTitle = `${scope} ~ ${parseInt(scope) + 500}`;
+
+    const title = `${typeTitle}${formatMessage({
+      id: "kasegi.title"
+    })} ${scopeTitle}`;
     return (
       <div style={styles.kasegiPage}>
         <Helmet>
           <meta charSet="utf-8" />
           <meta name="description" content={`TODO`} />
-          <title>
-            {scopeTitle}
-            {formatMessage({ id: "kasegi.title" })}
-          </title>
+          <title>{`${title} | Gitadora Skill Viewer`}</title>
         </Helmet>
+        <h1 style={styles.title}>{title}</h1>
         {data && data.other && (
           <div style={styles.notLastDiv}>
-            <div style={styles.caption}>
-              {`${typeTitle} OTHER (${scopeTitle})`}
-            </div>
+            <div style={styles.caption}>{`${typeTitleShort} OTHER`}</div>
             <KasegiTable data={data.other} />
           </div>
         )}
         {data && data.hot && (
           <div>
-            <div style={styles.caption}>
-              {`${typeTitle} HOT (${scopeTitle})`}
-            </div>
+            <div style={styles.caption}>{`${typeTitleShort} HOT`}</div>
             <KasegiTable data={data.hot} />
           </div>
         )}
@@ -104,6 +103,9 @@ const styles = {
   kasegiPage: {
     maxWidth: 800,
     fontFamily: "verdana" // TODO move this to global css? or app component
+  },
+  title: {
+    fontSize: 24
   },
   notLastDiv: {
     marginBottom: 20
