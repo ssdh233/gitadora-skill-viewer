d1480b68a2500f7238e0de5cb6762c06fd7611bf
matsumatsu233 <cuizhen199209@gmail.com>
2018-11-26 22:45:30 +0900

    v1.10.2

--- src/react/AppHeader.jsx
+++ src/react/AppHeader.jsx
@@ -12,7 +12,7 @@ import AttachMoney from "@material-ui/icons/AttachMoney";
 import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
 import Language from "@material-ui/icons/Language";
 
-const VERSION = "v1.10.1";
+const VERSION = "v1.10.2";
 
 class AppHeader extends React.Component {
   constructor(props) {
