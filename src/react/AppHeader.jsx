55b509a3c4671987c0470ad4e7d611206cb403b4
matsumatsu233 <cuizhen199209@gmail.com>
2019-01-03 18:43:18 +0900

    v1.14.5

--- src/react/AppHeader.jsx
+++ src/react/AppHeader.jsx
@@ -17,7 +17,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from "@material-ui/core/ListSubheader";
 
-const VERSION = "v1.14.4";
+const VERSION = "v1.14.5";
 
 class AppHeader extends React.Component {
   constructor(props) {
