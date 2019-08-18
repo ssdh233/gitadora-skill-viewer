bca3039cf09d7dd84b80dc59f9ea3358bd31ffcb
matsumatsu233 <cuizhen199209@gmail.com>
2019-08-18 20:56:31 +0900

    Preload css

--- src/views/htmlTemplate.js
+++ src/views/htmlTemplate.js
@@ -12,13 +12,14 @@ const htmlTemplate = ({
 <!doctype html>
 <html ${helmet.htmlAttributes.toString()}>
   <head>
+    <link rel="preload" href="https://unpkg.com/react-table@latest/react-table.css" as="style">
+    <link rel="preload" href="/bundle/${bundleFileName}" as="script" />
     <link rel="stylesheet" href="https://unpkg.com/react-table@latest/react-table.css" />
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Andada" />
     ${helmet.title.toString()}
     ${helmet.meta.toString()}
     ${helmet.link.toString()}
     ${helmet.style.toString()}
-    <link rel="preload" href="/bundle/${bundleFileName}" as="script" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="google-site-verification" content="${googleSiteVerfication}" />
     <script type='text/javascript' src="http://cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js" defer></script>
