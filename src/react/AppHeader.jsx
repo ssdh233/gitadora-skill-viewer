50578133ab52422849737978e062af3d921b9c66
matsumatsu233 <cuizhen199209@gmail.com>
2019-10-05 17:58:19 +0900

    v1.22.0

--- src/react/AppHeader.jsx
+++ src/react/AppHeader.jsx
@@ -22,7 +22,7 @@ import HomeIcon from "@material-ui/icons/Home";
 import useLocalStorage from "./useLocalStorage";
 import { CURRENT_VERSION } from "../constants";
 
-const VERSION = "v1.21.2";
+const VERSION = "v1.22.0";
 
 function AppHeader(props) {
   const [listAnchorEl, setListAnchorEl] = useState();
