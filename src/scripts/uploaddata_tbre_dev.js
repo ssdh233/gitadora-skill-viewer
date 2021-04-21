a916ed917d78cb725966117ac9400ee0ebcf7a3b
ssdh233 <songsongdahu233@gmail.com>
2021-04-22 05:25:03 +0900

    Fix nextage and tbre script

---
+++ src/scripts/uploaddata_tbre_dev.js
@@ -0,0 +1,7 @@
+import main from "./uploaddata_core";
+
+var TARGET_DOMAIN = "//gitadora-skill-viewer-dev.herokuapp.com";
+var SCRIPT_DOMAIN = TARGET_DOMAIN;
+var VERSION = "tbre";
+
+main(TARGET_DOMAIN, SCRIPT_DOMAIN, VERSION);
