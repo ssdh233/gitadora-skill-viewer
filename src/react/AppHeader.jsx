f03fa75a432245e8ec0fdf930c5024481f448439
ssdh233 <songsongdahu233@gmail.com>
2022-12-19 22:25:39 +0900

    Fix saveSkill

--- src/react/AppHeader.jsx
+++ src/react/AppHeader.jsx
@@ -22,7 +22,7 @@ import HomeIcon from "@material-ui/icons/Home";
 import useLocalStorage from "./useLocalStorage";
 import { CURRENT_VERSION } from "../constants";
 
-const VERSION = "v1.30.0";
+const VERSION = "v1.30.1";
 
 function AppHeader(props) {
   const [listAnchorEl, setListAnchorEl] = useState();
