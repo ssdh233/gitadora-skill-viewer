c4abdd6677e785bfb93226f2d035a8cd47f82707
ssdh233 <songsongdahu233@gmail.com>
2024-02-07 03:52:08 +0900

    Add out-of-date info for old kasegi pages

--- src/react/KasegiPage/KasegiPage.jsx
+++ src/react/KasegiPage/KasegiPage.jsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 import { injectIntl, FormattedMessage } from "react-intl";
 
 import KasegiTable from "./KasegiTable.jsx";
+import { CURRENT_VERSION, VERSION_NAME } from "../../constants.js";
 
 class KasegiPage extends React.Component {
   render() {
@@ -45,9 +46,20 @@ class KasegiPage extends React.Component {
             )}
           />
           <title>{`${title} | Gitadora Skill Viewer`}</title>
+          {
+            version !== CURRENT_VERSION && <meta name="robots" content="noindex" />
+          }
         </Helmet>
         <div style={styles.kasegiPage}>
           <h1 style={styles.title}>{title}</h1>
+          {version !== CURRENT_VERSION && <blockquote style={{
+            borderLeft: "5px solid #ffe564",
+            backgroundColor: "rgba(255,229,100,0.2)",
+            padding: "20px 24px",
+            margin: "24px 0"
+          }}>
+            ⚠️古いバージョンの情報です。最新の稼ぎ曲の情報は<a href={`/${locale}/${CURRENT_VERSION}/kasegi/${type}/${scope}`}>こちら</a>
+          </blockquote>}
           {comparedSkillData && (
             <h2 style={styles.subtitle}>
               {/* TODO add link after find a way to inject query to prop */}
