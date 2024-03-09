5a558a6b0e9529e6f1cf408b27a14af8456742bc
ssdh233 <songsongdahu233@gmail.com>
2024-03-10 02:57:00 +0900

    v1.37.0

--- src/react/AppHeader.jsx
+++ src/react/AppHeader.jsx
@@ -25,7 +25,7 @@ import { CURRENT_VERSION } from "../constants";
 import { versionInfo } from "graphql";
 import { withTheme } from "styled-components";
 
-const VERSION = "v1.36.2";
+const VERSION = "v1.37.0";
 
 function AppHeader(props) {
   const [langAnchorEl, setLangAnchorEl] = useState();
